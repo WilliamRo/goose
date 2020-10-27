83c622ecfff185a32ebd89bbad1cd79465db48aa
William <willi4m@zju.edu.cn>
2020-10-27 15:35:49 +0800

    [A] added files for PYPI; [M] modified .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+  name='goose',
+  packages=['goose'],
+  version='1.0.0',
+  description='GUA GUA GUA!',
+  author='William Ro',
+  author_email='willi4m@zju.edu.cn',
+  url='https://github.com/WilliamRo/goose',
+  download_url='https://github.com/WilliamRo/goose/tarball/v1.0.0',
+  keywords=['cloud', 'distributed'],
+  classifiers=[],
+  license='Apache-2.0',
+)
